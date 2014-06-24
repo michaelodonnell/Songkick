ead2ac9e7ac2b3fb0d00a267bd4b7c6f0c0b5073
mickod2000 <mail@michaelodonnell.ie>
2014-06-24 09:44:59 +0200

    Stable datepicker.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,14 +8,18 @@ songKickControllers.controller('ConcertListCtrl', ['$scope', '$http',
 
   	function ($scope, $http) {
 	  	$scope.APIKey = '2zTp3T1kEaz0aT1k';
-	  	$scope.selectedLocation = '82.124.24.137';
+      $scope.selectedLocation = '82.124.24.137';
+      $scope.minDate = '2014-06-01';
+      $scope.maxDate = '2015-06-01';
+      $scope.songKickRequest = '';
 
-	    $http.get('https://api.songkick.com/api/3.0/search/locations.json?location=clientip&apikey=' + $scope.APIKey).success(function(data) {
+	    $http.get('https://api.songkick.com/api/3.0/events.json?apikey=' + $scope.APIKey + '&location=ip:' + $scope.selectedLocation + '&min_date=' + $scope.minDate + '&max_date=' + $scope.maxDate).success(function(data) {
 	    $scope.locations = data;
     });
 
     var updateConcerts = function() {
-    	$http.get('https://api.songkick.com/api/3.0/events.json?apikey=' + $scope.APIKey + '&location=ip:' + $scope.selectedLocation + '&min_date=max_date=').success(function(data) {
+      // alert('https://api.songkick.com/api/3.0/events.json?apikey=' + $scope.APIKey + '&location=ip:' + $scope.selectedLocation + '&min_date' + $scope.minDate + '=max_date=' + $scope.maxDate);
+    	$http.get('https://api.songkick.com/api/3.0/events.json?apikey=' + $scope.APIKey + '&location=ip:' + $scope.selectedLocation + '&min_date=' + $scope.minDate + '&max_date=' + $scope.maxDate).success(function(data) {
 	    	$scope.concerts = data;
 	    });
     }
@@ -24,14 +28,14 @@ songKickControllers.controller('ConcertListCtrl', ['$scope', '$http',
       // alert($scope.dateRange.startDate);
 
       // Parse the start and end dates:
-      if(typeof $scope.dateRange.startDate != "undefined") {
+      /*if(typeof $scope.dateRange.startDate != "undefined") {3
         var month = $scope.dateRange.startDate.getUTCMonth();
         var day = $scope.dateRange.startDate.getUTCDate();
         var year = $scope.dateRange.startDate.getUTCFullYear();
 
         alert(year + "/" + month + "/" + day);
-      }
-      
+      }*/
+
       // $scope.dateRange.startDate.split("T").
     }
 
